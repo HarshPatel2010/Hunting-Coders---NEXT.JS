fe1c9025b7ef9545395e38e5105a1e25c83be36f
HarshPatel31 <harshpatel29301@gmail.com>
2022-05-11 02:43:10 +0530

    rendered navbar in all components means when you click on about so displayed navbar in about components too

---
+++ COMPONENTS/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from "next/link"
+import styles from '../styles/Home.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.mainNav} >
+    <ul >
+      <li><Link href="/about">Home</Link></li>
+      <li><Link href="/about">About</Link></li>
+      <li><Link href="/blog">Blog</Link></li>
+      <li><Link href="/contact">Contact</Link></li>
+     {/* <Link href="/about"> <li>About</li></Link>
+     <Link href="/blog"> <li>Blog</li></Link>
+     <Link href="/contact"> <li>Contact</li></Link> */}
+    </ul>
+  </nav>
+  )
+}
+
+export default Navbar
